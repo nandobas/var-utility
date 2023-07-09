4b1259954e486856a0cf13fc00fb87c555e4826a
nandobas <fernando.basilio@gmail.com>
2023-07-09 12:38:18 -0300

    mountains

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Live Video Streaming Controller</h1>
+    <p>Live video streaming controller for light demand</p>
+    <p>Developed by: Basilio, Fernando</p>
+    <v-btn href="https://github.com/nandobas" target="_blank" text>
+          <span>GitHub Repository</span>
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
   </div>
 </template>
